cf09d0e0025c7b070df07753e60beea866c65b1b
AnilCM <56347169+Anil-CM@users.noreply.github.com>
2021-05-30 23:41:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,21 @@
-resource "null_resource" "env_workspace" {
-  provisioner "local-exec" {
-    command = "echo Hello ${var.name}"
-  }
+
+locals {
+  input_name = "Hello ${var.name}"
+  output_age = "${var.name}'s Age is ${var.age}"
+}
+
+
+# resource "null_resource" "env_workspace" {
+#   provisioner "local-exec" {
+#     command = "echo Hello ${var.name}"
+#     timeout = 20
+#   }
+# }
+
+output "one" {
+  value = "${local.input_name}"
+}
+
+output "two" {
+  value = "${local.output_age}"
 }
